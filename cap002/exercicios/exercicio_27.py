ccc8db9a48fb47ae4b1188eec8f4939150e886dc
Adelmo Dhelldev <dhelldev@gmail.com>
2025-10-30 18:52:13 -0300

    Capítulo 2 - 2.8 Uma rápida revisão de conceitos matemáticos

---
+++ cap002/exercicios/exercicio_27.py
@@ -0,0 +1,6 @@
+# Exercício 2.7
+"""
+Usando as propriedades da divisão e da multiplicação, 
+tente entender como estes resultados são iguais:
+"""
+print(f"{6.0 * 0.2 + 8.0 * 0.3 + 7.0 * 0.5} = {(6.0 * 20 + 8.0 * 30 + 7.0 * 50) / 100}")
