d64fa5b56eba21769f03a1a2e8ea612f8a9b16d4
dhelldev <adelmo.msp@gmail.com>
2025-11-25 09:13:59 -0300

    Capítulo 3 - 3.5 Sequências e tempo

---
+++ cap003/programas/programa_31.py
@@ -0,0 +1,14 @@
+# Programa 3.1 - Exemplo de sequência e tempo
+
+divida = 0
+compra = 100
+divida = divida + compra
+
+compra = 200
+divida = divida + compra
+
+compra = 300
+divida = divida + compra
+
+compra = 0
+print(divida)
